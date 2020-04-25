5832b521e40a28263853e526a3a55561e710023b
AE173 <s31079@tichon.shohamedu.org.il>
2020-04-25 11:00:23 +0300

    Added BaseTalonEncoder class for TalonSrx AND TalonFx, fixed gradle

---
+++ src/sensors/counter/BaseTalonEncoder.java
@@ -0,0 +1,28 @@
+package sensors.counter;
+
+import com.ctre.phoenix.motorcontrol.can.BaseTalon;
+
+public class BaseTalonEncoder implements Counter {
+  private final BaseTalon baseTalon;
+  private final int pidSlot;
+
+  public BaseTalonEncoder(BaseTalon baseTalon, int pidSlot) {
+    this.baseTalon = baseTalon;
+    this.pidSlot = pidSlot;
+  }
+
+  @Override
+  public int getCount() {
+    return baseTalon.getSelectedSensorPosition(pidSlot);
+  }
+
+  @Override
+  public double getRate() {
+    return baseTalon.getSelectedSensorVelocity(pidSlot);
+  }
+
+  @Override
+  public void reset() {
+    baseTalon.setSelectedSensorPosition(0, pidSlot, 100);
+  }
+}
