e811f1f2d41c647a947acb28f7a85b2aa79d663a
AE173 <s31079@tichon.shohamedu.org.il>
2020-04-25 11:38:07 +0300

    Removed all of the m_  prefixes from the names

--- src/sensors/counter/RoboRioEncoder.java
+++ src/sensors/counter/RoboRioEncoder.java
@@ -3,26 +3,26 @@ package sensors.counter;
 import edu.wpi.first.wpilibj.Encoder;
 
 public class RoboRioEncoder implements Counter {
-    private final Encoder m_encoder;
-    private final double m_distancePerUnit;
+    private final Encoder encoder;
+    private final double distancePerUnit;
 
     public RoboRioEncoder(final Encoder encoder, final double distancePerUnit) {
-        m_encoder = encoder;
-        m_distancePerUnit = distancePerUnit;
+        this.encoder = encoder;
+        this.distancePerUnit = distancePerUnit;
     }
 
     @Override
     public int getCount() {
-        return m_encoder.get();
+        return encoder.get();
     }
 
     @Override
     public double getRate() {
-        return m_encoder.getRate();
+        return encoder.getRate();
     }
 
     @Override
     public void reset() {
-        m_encoder.reset();
+        encoder.reset();
     }
 }
--- src/sensors/counter/TalonSrxEncoder.java
+++ src/sensors/counter/TalonSrxEncoder.java
@@ -3,26 +3,26 @@ package sensors.counter;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class TalonSrxEncoder implements Counter {
-    private final TalonSRX m_talonSRX;
-    private final int m_pidSlot;
+    private final TalonSRX talonSRX;
+    private final int pidSlot;
 
     public TalonSrxEncoder(final TalonSRX talonSRX, final int pidSlot) {
-        m_talonSRX = talonSRX;
-        m_pidSlot = pidSlot;
+        this.talonSRX = talonSRX;
+        this.pidSlot = pidSlot;
     }
 
     @Override
     public int getCount() {
-        return m_talonSRX.getSelectedSensorPosition(m_pidSlot);
+        return talonSRX.getSelectedSensorPosition(pidSlot);
     }
 
     @Override
     public double getRate() {
-        return m_talonSRX.getSelectedSensorVelocity(m_pidSlot);
+        return talonSRX.getSelectedSensorVelocity(pidSlot);
     }
 
     @Override
     public void reset() {
-        m_talonSRX.setSelectedSensorPosition(0, m_pidSlot, 100);
+        talonSRX.setSelectedSensorPosition(0, pidSlot, 100);
     }
 }
