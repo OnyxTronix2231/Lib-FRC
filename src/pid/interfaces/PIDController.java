5a39ff1197d66736aae33fb3b14b28fc911679c6
AE173 <s31079@tichon.shohamedu.org.il>
2020-11-16 20:59:26 +0200

    deleted doubled functions in Controller and PIDController

--- src/pid/interfaces/PIDController.java
+++ src/pid/interfaces/PIDController.java
@@ -1,12 +1,6 @@
 package pid.interfaces;
 
-import pid.CustomizeControlMode;
-
 public interface PIDController extends Controller {
-  double getSetpoint();
-
-  void setSetpoint(double setpoint);
-
   double getProcessVariable();
 
   double getCurrentError();
