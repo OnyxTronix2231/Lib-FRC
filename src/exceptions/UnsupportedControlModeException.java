96b91c6ead1b3aa028f089c34970d588f38c7e23
AE173 <s31079@tichon.shohamedu.org.il>
2020-12-26 09:15:28 +0200

    UnsupportedControlModeException now extends RuntimeException and not Exception

--- src/exceptions/UnsupportedControlModeException.java
+++ src/exceptions/UnsupportedControlModeException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class UnsupportedControlModeException extends Exception {
+public class UnsupportedControlModeException extends RuntimeException {
   public UnsupportedControlModeException(){
     super("This control mode is not supported." +
         " Only supported control modes are position and velocity");
