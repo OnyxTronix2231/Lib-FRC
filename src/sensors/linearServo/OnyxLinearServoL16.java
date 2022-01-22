650653dda07214305cf12d2a5ebc204d1ee51c24
YuvalRashman <yrashman@gmail.com>
2022-01-22 17:33:52 +0200

    add OnyxLibary

--- src/sensors/linearServo/OnyxLinearServoL16.java
+++ src/sensors/linearServo/OnyxLinearServoL16.java
@@ -4,63 +4,57 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Timer;
 
-public class LinearServoL16 extends Servo {
+public class OnyxLinearServoL16 extends Servo {
 
-    double m_speed;
-    double m_length;
-    double setPos;
-    double curPos;
+    private final double m_speed;
+    private final double m_length;
+    private double setPos;
+    private double curPos;
+
 
     /**
+     * Constructor.<br>
+     *
+     * <p>By default {@value #kDefaultMaxServoPWM} ms is used as the maxPWM value<br>
+     * By default {@value #kDefaultMinServoPWM} ms is used as the minPWM value<br>
+     *
+     * @param channel The PWM channel to which the servo is attached. 0-9 are on-board, 10-19 are on
+     *                the MXP port
+     *
      * Parameters for L16-R Actuonix Linear Actuators
      *
      * @param channel PWM channel used to control the servo
-     * @param length  max length of the servo [mm]
-     * @param speed   max speed of the servo [mm/second]
+     * @param length max length of the servo [mm]
+     * @param speed max speed of the servo [mm/second]
      */
-    public LinearServoL16(int channel, int length, int speed) {
+    public OnyxLinearServoL16(int channel, int length, int speed) {
         super(channel);
         setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
         m_length = length;
         m_speed = speed;
-    }
 
+    }
     /**
-     * Miniature Linear Servo Actuators -
-     * User Guide (Rev 1)
-     * Table of Contents Page 10
-     * wcproducts.com
-     * <p>
      * Run this method in any periodic function to update the position estimation of your
-     * servo
+     servo
      *
-     * @param setpoint the target position of the servo [mm]
+     * @param setPoint the target position of the servo [mm]
      */
-    public void setPosition(double setpoint) {
-        setPos = MathUtil.clamp(setpoint, 0, m_length);
-        setSpeed((setPos / m_length * 2) - 1);
+    public void setPosition(double setPoint) {
+        setPos = MathUtil.clamp(setPoint, 0, m_length);
+        setSpeed( (setPos/m_length * 2)-1);
     }
-
-    double lastTime = 0;
-
     /**
      * Run this method in any periodic function to update the position estimation of your
-     * servo
+     servo
      */
     public void updateCurPos() {
-        double dt = Timer.getFPGATimestamp() - lastTime;
-        if (curPos > setPos + m_speed * dt) {
-            curPos -= m_speed * dt;
-        } else if (curPos < setPos - m_speed * dt) {
-            curPos += m_speed * dt;
-        } else {
-            curPos = setPos;
-        }
-    }
+        curPos = m_length * this.get();
 
+    }
     /**
      * Current position of the servo, must be calling {@link #updateCurPos()
-     * updateCurPos()} periodically
+    updateCurPos()} periodically
      *
      * @return Servo Position [mm]
      */
@@ -70,12 +64,10 @@ public class LinearServoL16 extends Servo {
 
     /**
      * Checks if the servo is at its target position, must be calling {@link #updateCurPos()
-     * updateCurPos()} periodically
-     *
+    updateCurPos()} periodically
      * @return true when servo is at its target
      */
     public boolean isFinished() {
         return curPos == setPos;
     }
 }
-
