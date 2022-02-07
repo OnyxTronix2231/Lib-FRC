128011b23c7b73a0c64aa18fb0f974f822e5f11f
YuvalRashman <yrashman@gmail.com>
2022-02-07 19:01:11 +0200

    fix funcsions

--- src/sensors/counter/CtreCANCoder.java
+++ src/sensors/counter/CtreCANCoder.java
@@ -2,28 +2,42 @@ package sensors.counter;
 
 import com.ctre.phoenix.sensors.CANCoder;
 
+import static pid.CtreConstants.CTRE_DEVICE_CALLS_TIMEOUT;
+
 public class CtreCANCoder extends CANCoder implements CtreEncoder {
 
-    public CtreCANCoder(int deviceNumber, String canbus) {
+    private final int timeoutResetMs;
+
+    public CtreCANCoder(int deviceNumber, String canbus, int timeoutResetMs) {
         super(deviceNumber, canbus);
+        this.timeoutResetMs = timeoutResetMs;
+    }
+
+    public CtreCANCoder(int deviceNumber, String canbus) {
+        this(deviceNumber, canbus, CTRE_DEVICE_CALLS_TIMEOUT);
     }
 
     public CtreCANCoder(int deviceNumber) {
+        this(deviceNumber, CTRE_DEVICE_CALLS_TIMEOUT);
+    }
+
+    public CtreCANCoder(int deviceNumber, int timeoutResetMs) {
         super(deviceNumber);
+        this.timeoutResetMs = timeoutResetMs;
     }
 
     @Override
     public double getCount() {
-        return this.getCount();
+        return getPosition();
     }
 
     @Override
     public double getRate() {
-        return this.getRate();
+        return getVelocity();
     }
 
     @Override
     public void reset() {
-        this.reset();
+        setPosition(0, timeoutResetMs);
     }
 }
