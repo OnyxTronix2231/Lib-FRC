b36bdaf4949ff44835eec6ae4cf0224ae8a9c314
YuvalRashman <yrashman@gmail.com>
2022-02-11 00:16:32 +0200

    delete s

--- src/sensors/triangulatingRangefinder/TriangulatingRangefinder.java
+++ src/sensors/triangulatingRangefinder/TriangulatingRangefinder.java
@@ -1,8 +1,8 @@
-package sensors.triangulatingRangefinders;
+package sensors.triangulatingRangefinder;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 
-public class TriangulatingRangefinders extends AnalogInput {
+public class TriangulatingRangefinder extends AnalogInput {
 
     private final static double FORMULA_MULTIPLIER_CONSTANT = 29.988;
     private final static double FORMULA_POW_CONSTANT = -1.173;
@@ -11,7 +11,7 @@ public class TriangulatingRangefinders extends AnalogInput {
      *this class work with GP2Y0A02YK0F IR sensor,
      * @param channel The channel number to represent. 0-3 are on-board 4-7 are on the MXP port.
      */
-    public TriangulatingRangefinders(int channel) {
+    public TriangulatingRangefinder(int channel) {
         super(channel);
     }
 
