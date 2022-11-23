3fcbb5360bbc2050f8c7caf344a3bf5cf2c9f886
killery2201 <nivyuval11@gmail.com>
2022-11-23 19:39:58 +0200

    lazy classes for motors

---
+++ src/motors/LazyTalonSRX.java
@@ -0,0 +1,26 @@
+package motors;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class LazyTalonSRX extends WPI_TalonSRX {
+    protected double mLastSet = Double.NaN;
+    protected ControlMode mLastControlMode = null;
+
+    public LazyTalonSRX(int deviceNumber) {
+        super(deviceNumber);
+    }
+
+    public double getLastSet() {
+        return mLastSet;
+    }
+
+    @Override
+    public void set(ControlMode mode, double value) {
+        if (value != mLastSet || mode != mLastControlMode) {
+            mLastSet = value;
+            mLastControlMode = mode;
+            super.set(mode, value);
+        }
+    }
+}
