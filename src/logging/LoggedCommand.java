63be4aa4be09319fffc07d9092dde6db1d884a7e
negbi <itzhakn@protonmail.com>
2023-12-29 15:57:20 +0200

    cr

--- src/logging/LoggedCommand.java
+++ src/logging/LoggedCommand.java
@@ -2,27 +2,25 @@ package logging;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class LoggedComand extends Command {
+public class LoggedCommand extends Command {
 
     public static boolean IS_LOG = false;
 
-    private String commandName = getClass().toString();
+    private String name = getClass().toString();
     private String requirements = getRequirements().toString();
 
     @Override
     public void initialize() {
         if (IS_LOG) {
-            System.out.println("[" + System.currentTimeMillis() + "] - (" + requirements + ") " + commandName);
+            System.out.println("[" + System.currentTimeMillis() + "] - (" + requirements + ") " + name);
             log();
         }
-
     }
 
     @Override
     public void end(boolean interrupted) {
         if (IS_LOG) {
-            System.out
-                    .println("[" + System.currentTimeMillis() + "] - (" + requirements + ") " + commandName + " - end");
+            System.out.println("[" + System.currentTimeMillis() + "] - (" + requirements + ") " + name + " - end");
             log();
         }
     }
